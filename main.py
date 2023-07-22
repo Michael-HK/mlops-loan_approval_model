b47b92c88b05366a37fc5ef1226b1c5fc586c78e
Alabi, Tobi Michael <56853529+Michael-HK@users.noreply.github.com>
2023-07-22 20:08:10 +0800

    Update and rename Flask.py to main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import pickle
+import pandas as pd
+import request
 
 # prediction function
 def Loan_approval_predictor(to_predict_list):
     
-    #to_predict = np.array(to_predict_list).reshape(1, len(to_predict_list))
-    df = pd.DataFrame(to_predict_list)       # to_predict_list is a dict
+    df = pd.DataFrame.from_dict(to_predict_list)       # to_predict_list is a dict
   
     numeric_cols = df['ApplicantIncome', 'CoapplicantIncome', 'LoanAmount', 'Loan_Amount_Term']
     df_encode = df[['Education',' Dependents', 'Self_Employed', 'Gender', 'Married', 'Property_Area']]
@@ -14,21 +15,20 @@ def Loan_approval_predictor(to_predict_list):
     df['Education',' Dependents', 'Self_Employed', 'Gender', 'Married', 'Property_Area'] = df_encode.apply(le.transform)
   
     # scaling input value
+    scaler = pickle.load(open("scaler_input.pkl", "rb"))
     df['ApplicantIncome', 'CoapplicantIncome', 'LoanAmount', 'Loan_Amount_Term'] = numeric_cols.apply(scaler.transform)
     loaded_model = pickle.load(open("loan_model.pkl", "rb"))
-    result = loaded_model.predict(df.values)
+    result = loaded_model.predict(df.to_numpy().tolist())
     return result[0]
  
-@app.route('/result', methods = ['POST'])
-def result():
+@app.route('/predict', methods = ['POST'])
+def predict():
     if request.method == 'POST':
-        to_predict_list = request.form.to_dict()
-        #to_predict_list = list(to_predict_list.values())
-        #to_predict_list = list(map(int, to_predict_list))
-        result = Loan_approval_predictor(to_predict_list)       
+        predict_list = request.form.to_dict()
+        result = Loan_approval_predictor(predict_list)       
         if int(result)== 1:
-            prediction ='Approved Loan'
+            prediction ='Loan Approved'
         else:
-            prediction ='Loan denied! You dont meet the requirements'     
-        return render_template("result.html", prediction = prediction)    
-        return render_template("result.html", prediction = prediction)
+            prediction ="Loan denied! You don't meet the requirements"     
+        return render_template(result.html, prediction = prediction)    
+        return render_template(result.html, prediction = prediction)
